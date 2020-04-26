1bdd43296f3c987d0701218935e05c36d7c3f3ca
jordi <jordihortal@outlook.es>
2020-04-27 01:17:44 +0200

    mysql driver add as dependency

--- src/test/java/TestLibroDAOImp.java
+++ src/test/java/TestLibroDAOImp.java
@@ -7,6 +7,7 @@ public class TestLibroDAOImp {
     @Test
     public void testLibroDAO(){
         DAO<Libro> libro = DAOFactory.getInstance().getLibroDAO();
-        libro.find(1);
+        Libro l = libro.find(1);
+        System.out.printf(l.getTitulo());
     }
 }
