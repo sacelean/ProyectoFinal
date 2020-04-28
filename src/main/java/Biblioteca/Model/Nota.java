cef87ec9f8312686d0c55eb4afc4605b2722edb3
Nataliariosrod <natriorod@gmail.com>
2020-04-28 14:43:16 +0200

    Clase Nota

---
+++ src/main/java/Biblioteca/Model/Nota.java
@@ -0,0 +1,10 @@
+package Biblioteca.Model;
+
+public class Nota {
+
+    private int idNota;
+    private String tema;
+
+    //Una o varias notas pueden ser de un tema.
+    //Un libro o articulo pueden tener una o varias notas
+}
