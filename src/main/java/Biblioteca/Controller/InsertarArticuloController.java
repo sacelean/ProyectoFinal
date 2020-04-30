178f40d6b291b8a4453f0a66176dbf498e27a351
Nataliariosrod <natriorod@gmail.com>
2020-04-30 13:33:01 +0200

    InsertarLibro con ids

--- src/main/java/Biblioteca/Controller/InsertarArticuloController.java
+++ src/main/java/Biblioteca/Controller/InsertarArticuloController.java
@@ -1,4 +1,5 @@
 package Biblioteca.Controller;
 
 public class InsertarArticuloController {
+
 }
--- src/main/java/Biblioteca/Controller/InsertarLibroController.java
+++ src/main/java/Biblioteca/Controller/InsertarLibroController.java
@@ -1,19 +1,44 @@
 package Biblioteca.Controller;
 
 import Biblioteca.Main;
+import com.jfoenix.controls.JFXComboBox;
 import com.jfoenix.controls.JFXTextField;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.VBox;
+
+import java.awt.*;
 
 public class InsertarLibroController {
 
-    @FXML private JFXTextField txtTitulo;
+
+    @FXML private AnchorPane anchorPaneLibro;
+    @FXML private VBox vBoxLibro;
+    @FXML private GridPane gridPaneLibro;
+    @FXML private Label lblRegistroLibro;
+    @FXML private Label lblLibroArticulo;
+    @FXML private Label lblTitulo;
+    @FXML private Label lblAutor;
+    @FXML private Label lblEditorial;
+    @FXML private Label lblAnio;
+    @FXML private Label lblNumPag;
+    @FXML private Label lblISBN;
+    @FXML private JFXComboBox JComboBox;
+    @FXML private JFXTextField JTxtTitulo;
+    @FXML private JFXTextField JTxtAutor;
+    @FXML private JFXTextField JTxtEditorial;
+    @FXML private JFXTextField JTxtAnio;
+    @FXML private JFXTextField JTxtNumPag;
+    @FXML private JFXTextField JTxtISBN;
+    private Main mainApp;
+
 
     public InsertarLibroController(){
 
     }
-    private Main mainApp;
-
 
     public void setMainApp(Main mainApp){
         this.mainApp = mainApp;
@@ -22,4 +47,7 @@ public class InsertarLibroController {
     public void handleCrear(ActionEvent actionEvent) {
 
     }
+
+
+
 }
